e0ba63af9b17859ca58beafbbe910d36918cbe08
Joel Del Cueto <wayfaringjou@gmail.com>
2020-10-27 17:25:13 -0400

    Add displaying image functionality

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 'use-strict';
 
+function generateImageListItem(item) {
+  return `
+  <li>
+  <img src="${item}" />
+  </li>
+  `;
+}
+// Use a map function to generate list items
+function generateImageList(data) {
+  const imageList = data.message.map((item) => generateImageListItem(item));
+  return `
+  <ul>
+    ${imageList.join('')}
+  </ul>
+  `;
+}
+// Render image list inside #image-display section
+function renderImages(data) {
+  $('main').find('#image-display').html(generateImageList(data));
+}
+
 // Fetch request to dogAPI
 function getDogImages(imageNumber) {
   const options = { method: 'GET' };
   fetch(`https://dog.ceo/api/breeds/image/random/${imageNumber}`, options)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    // Call renderImage function with response data
+    .then((data) => renderImages(data))
     .catch((error) => alert('An error has ocurred, please try later.'));
 }
 
