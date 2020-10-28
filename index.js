24b980d347305759db1b642776160fec983c676f
Joel Del Cueto <wayfaringjou@gmail.com>
2020-10-28 11:40:39 -0400

    Listen to submit instead of click. Add clearer instructions to user

--- index.js
+++ index.js
@@ -33,18 +33,9 @@ function getDogImages(imageNumber) {
 
 // Listen to form submission and pass input value to fetch request
 function submitClickedHandler() {
-  $('main').on('click', '.js-image-submit', function (e) {
+  $('main').on('submit', '.js-image-form', function (e) {
     e.preventDefault();
-    // Math.trunc is used to handle inputs with periods entered by keyboard
-    let imageNumber = Math.trunc($(this).siblings('#image-number').val());
-    // If lower than 1 or higher than 50 is entered by keyboard, reset to min and max
-    if (imageNumber <= 0) {
-      imageNumber = 1;
-      $(this).siblings('#image-number').val(imageNumber);
-    } else if (imageNumber > 50) {
-      imageNumber = 50;
-      $(this).siblings('#image-number').val(imageNumber);
-    }
+    const imageNumber = $(this).find('#image-number').val();
     getDogImages(imageNumber);
   });
 }
