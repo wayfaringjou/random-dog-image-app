c3d7960cc4fb159c4ecfeb1166688c0792ceb5d7
Joel Del Cueto <wayfaringjou@gmail.com>
2020-10-30 11:29:40 -0400

    Add breed name caption. Add header for image section

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 'use-strict';
 
-function generateImageListItem(item) {
+function generateImageListItem(item, breedArr) {
+  const searchPos = item.indexOf('breeds/') + 'breeds/'.length;
+  let breed = item.slice(searchPos, item.indexOf('/', searchPos));
+  if (breed.includes('-')) {
+    const breedComp = breed.split('-');
+    breed = `${breedComp[1]} ${breedComp[0]}`;
+  }
+  breedArr.push(breed);
   return `
   <li>
-  <img src="${item}" />
+  <img src="${item}" alt="${breed}"/>
+  <p class="img-caption capitalize">${breed}</p>
   </li>
   `;
 }
 // Use a map function to generate list items
 function generateImageList(data) {
-  const imageList = data.message.map((item) => generateImageListItem(item));
+  const breedArr = [];
+  const imageList = data.message.map((item) => generateImageListItem(item, breedArr));
+  let headerMsg;
+  if (breedArr.length > 2) {
+    headerMsg = `${breedArr[0]}, ${breedArr[1]}</span>, and more`;
+  } else if (breedArr.length > 1) {
+    headerMsg = `${breedArr[0]} and ${breedArr[1]}</span>`;
+  } else {
+    headerMsg = `${breedArr[0]}</span>`;
+  }
   return `
+  <h3>Look what we found! <span class="capitalize">${headerMsg}!</h3>
   <ul>
     ${imageList.join('')}
   </ul>
